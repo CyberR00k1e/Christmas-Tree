1c6dde521e55ad090a2e4fe0cdf1b13f87f58f62
srt <sharat.nautiyal@gmail.com>
2022-12-25 13:33:39 +0800

    Added time module

--- christmas.py
+++ christmas.py
@@ -4,16 +4,16 @@ import time
 r=15
 i=1
 
-
+cprint("Wishing all of you a Merry Christmas and Happy New Year from Vectra,Asia team"+ "\n", "green",)
 for i in range(r):
     cprint((" "*r + ("*"+" ")*i + " "*r),"red",)
     i=i+1
     r=r-1
-    time.sleep(0.5)
+    time.sleep(0.3)
 
 print("\n")
 time.sleep(0.2)
-cprint("Wishing all of you a Merry Christmas and Happy New Year from Vectra,Asia team"+ "\n", "blue","on_green")
+cprint("Merry Christmas and Happy New Year !", "green"+"\n")
 
 
 
